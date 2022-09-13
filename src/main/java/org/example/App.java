f7b7105793959f59bb6a02c9e63ee34eaa4a3418
satyainv <satyainv459@gmail.com>
2022-09-13 22:19:58 +0200

    Satya second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello satya World!" );
+        System.out.println("satya added more changes");
     }
 }
