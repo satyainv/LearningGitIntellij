82780433fc0fef5c2a9f2ec1db11032bd9af3aa1
satyainv <satyainv459@gmail.com>
2022-09-13 21:59:22 +0200

    Satya first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("hello satya this is test class");
+    }
+}
